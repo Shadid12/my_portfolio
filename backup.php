8c06960ea4f56cd84450796d3ab61ec9c7d51967
Shadid12 <shadid.haque@uoit.net>
2017-04-26 17:14:12 +0000

    coloumn fixed in experience

---
+++ backup.php
@@ -0,0 +1,59 @@
+<div class="columns" id="exp">
+          <div class="column is-offset-2" id="exC">
+            <div class="card">
+
+                <div class="card-image">
+                    <figure class="image is-6">
+                      <img src="http://www.logodesignlove.com/images/pictograms/ibm-logo-pictogram.jpg" alt="Image">
+                    </figure>
+                </div>
+                
+                <div class="card-content">
+                  <div class="media-content">
+                    <p class="title is-8">IBM Analytics</p> <br>
+                    <p class="subtitle is-8">Full Stack Developer / DevOps </p>
+                  </div>
+                  <hr>
+                  <div class="content">
+                      Developed web applications with Node.js, Ruby on Rails and Java. Developed rich client side UI/UX with Jquery, CSS/SASS and Angular.js framework.
+                      Also implemented REST API with<a>@Node.js</a> and <a>@Express.js</a>. Performed Test Driven Development for JavaScript with Jasmin, and RSpec for Rails.
+                      <a>#Rails</a> <a>#Node.js</a> <a>#Angular.js</a>
+                      <br>
+                      <small>May 2016 - Dec 2016</small>
+                  </div>
+                  
+                </div>
+
+
+            </div>
+          </div>
+          
+          <div class="column is-offset-2">
+            <div class="card">
+
+                <div class="card-image">
+                    <figure class="image is-6">
+                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQspAGIKkKBtXlsytaB59JpVdRq8b4l9yGZD02H4pgcphKBt2G_" alt="Image">
+                    </figure>
+                </div>
+                
+                <div class="card-content">
+                  <div class="media-content">
+                    <p class="title is-8">IBM Application Security on Cloud</p> <br>
+                    <p class="subtitle is-8"> Java Developer </p>
+                  </div>
+                  <hr>
+                  <div class="content">
+                      Developed Java REST API endpoints for microservice based web applications. Developed client side UI/UX code with javascript frameworks such as <a>Angular.js</a>
+                      and <a>React.js</a>
+                      <br>
+                      <small>May 2016 - Dec 2016</small>
+                  </div>
+                  
+                </div>
+
+
+            </div>
+          </div>
+          
+        </div>
