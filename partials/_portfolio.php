1554e2e1c1a52de125178eb84fc53a40aa0acfe9
Shadid12 <shadid.haque@uoit.net>
2017-05-02 23:45:48 +0000

    equal colum

--- partials/_portfolio.php
+++ partials/_portfolio.php
@@ -7,70 +7,32 @@
 
 <hr>
 
-<div class="columns">
-  <div class="column">
-    <!---->
-    <div class="card" id="projectCard">
+
+<div id="css-table">
+  <div class="col">
         <figure class="image is-4by3">
           <img src="img/mib2.jpg" alt="Image">
         </figure>
-        <div class="title"> Message in a Bottle</div>
-        <div class="content">
+      <div class="title"> Message in a Bottle</div>
+      <p>
             How romanctic is the idea to send a message in a bottle. How about if there was some kind of digital service like this. 
             People can put their Messages (text, photos) in a "bottle" and send 
             it out to the world. On the other side, people receive the messages 
             and can decide to either (A) keep the message or (B) throw it back.
             Build with <a>@Ruby on Rails</a> &nbsp <a>Jquery</a>and <a>@PostgresSQL</a>
             it is a new way to rethink social networks.
-        </div>
-        <div>
-            &nbsp&nbsp<a class="button is-success is-outlined" href="https://messageinbottle.herokuapp.com/">Live Demo</a>
-            &nbsp&nbsp<a class="button is-success is-outlined" href="https://github.com/Shadid12/Message_Bottle">Code &nbsp&nbsp<i class="fa fa-github" aria-hidden="true"></i></a>
-            &nbsp&nbsp<a class="button is-success is-outlined" href="https://medium.com/@shadidhaque/create-your-own-coding-boot-camp-72f45041cefd">Blog &nbsp&nbsp<i class="fa fa-medium" aria-hidden="true"></i></a>
-        </div>
-    </div>
-    <!---->
-    
+      </p>
+      <a class="button is-success is-outlined" href="https://messageinbottle.herokuapp.com/">Live Demo</a>
+      <a class="button is-success is-outlined" href="https://github.com/Shadid12/Message_Bottle">
+          Code &nbsp&nbsp<i class="fa fa-github" aria-hidden="true"></i>
+      </a>
+      <a class="button is-success is-outlined" href="https://medium.com/@shadidhaque/create-your-own-coding-boot-camp-72f45041cefd">
+      Blog &nbsp&nbsp<i class="fa fa-medium" aria-hidden="true"></i>
+      </a>
   </div>
-  <div class="column">
-    <!---->
-    <div class="card" id="projectCard">
-        <figure class="image is-4by3">
-          <img src="http://camsafesolution.org/wp-content/uploads/2016/08/site-under-construction.jpg" alt="Image">
-        </figure>
-        <div class="title">Tweet Snap</div>
-        <div class="content">
-            An application that lets you search and sort out your favourite twitter pictures and videos. Built using the Powerful <a>@Twitter API</a> and <a>React.js</a>
-            Tweet Snap provides a new way to tweeter exoerience. This project is still in development.
-            <a>#css</a> <a>#responsive</a>
-        </div>
-        <div>
-            &nbsp&nbsp<a class="button is-success is-outlined">Live Demo</a>
-            &nbsp&nbsp<a class="button is-success is-outlined">Code &nbsp&nbsp<i class="fa fa-github" aria-hidden="true"></i></a>
-            &nbsp&nbsp<a class="button is-success is-outlined">Blog &nbsp&nbsp<i class="fa fa-medium" aria-hidden="true"></i></a>
-        </div>
-    </div>
-    <!---->
-  </div>
-  <div class="column">
-    <!---->
-    <div class="card" id="projectCard">
-        <figure class="image is-4by3">
-          <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Image">
-        </figure>
-        <div class="content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-            <a>#css</a> <a>#responsive</a>
-        </div>
-        <div>
-            &nbsp&nbsp<a class="button is-success is-outlined">Live Demo</a>
-            &nbsp&nbsp<a class="button is-success is-outlined">Code &nbsp&nbsp<i class="fa fa-github" aria-hidden="true"></i></a>
-            &nbsp&nbsp<a class="button is-success is-outlined">Blog &nbsp&nbsp<i class="fa fa-medium" aria-hidden="true"></i></a>
-        </div>
-    </div>
-    <!---->
-  </div>
+  <div class="col"><p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p></div>
+  <div class="col"><p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p></div>
+  <div class="col"><p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p></div>
 </div>
 
 </div>
@@ -79,5 +41,20 @@
 <style type="text/css">
 #projectCard {
     height: 40px;
+}
+
+#css-table {
+  display: table;
+}
+#css-table .col {
+  display: table-cell;
+  width: 25%;
+  padding: 10px;
+}
+#css-table .col:nth-child(even) {
+  background: #ccc;
+}
+#css-table .col:nth-child(odd) {
+  background: #eee;
 }
 </style>
