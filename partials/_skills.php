75bae9707342f1451a7bf09ef3d88d7d09e748e4
Shadid12 <shadid.haque@uoit.net>
2017-05-08 14:54:15 +0000

    fixed

--- partials/_skills.php
+++ partials/_skills.php
@@ -1,17 +1,17 @@
 <div class="container is-fluid" id="skillicon">
 
 <figure class="snip1566">
-  <img src="http://www.unixstickers.com/image/cache/data/stickers/js/js-shaped-600x600.jpg" alt="sq-sample14" />
+  <img src="img/skills/js.jpg" alt="sq-sample14" />
   <figcaption><a><i class="ion-android-add"></i></a></figcaption>
 </figure>
 
 <figure class="snip1566">
-  <img src="https://pbs.twimg.com/media/CZGHPChUAAA3jqE.png" alt="sq-sample14" />
+  <img src="img/skills/rails.png" alt="sq-sample14" />
   <figcaption><i class="ion-android-add"></i></figcaption>
 </figure>
 
 <figure class="snip1566">
-  <img src="http://kodeinfo.com/uploads/images/1423349185_post.png" alt="sq-sample14" />
+  <img src="img/skills/php.png" alt="sq-sample14" />
   <figcaption><i class="ion-android-add"></i></figcaption>
 </figure>
 
@@ -26,12 +26,12 @@
 </figure>
 
 <figure class="snip1566">
-  <img src="http://www.unixstickers.com/image/cache/data/stickers/react/reactjs-shaped-600x600.jpg" alt="sq-sample14" />
+  <img src="img/skills/react.jpg" alt="sq-sample14" />
   <figcaption><i class="ion-android-add"></i></figcaption>
 </figure>
 
 <figure class="snip1566">
-  <img src="http://j-arellano.com/wp-content/uploads/2014/04/git.sh-600x600.png" alt="sq-sample14" />
+  <img src="img/skills/git.png" alt="sq-sample14" />
   <figcaption><i class="ion-android-add"></i></figcaption>
 </figure>
 
